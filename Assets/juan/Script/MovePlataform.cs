6b6f21c7b1d95c68509424ce73d7014594af967e
hellcrey <109318827+hellcrey@users.noreply.github.com>
2024-04-19 12:10:26 -0500

    enemy plataforma
    script movimiento de plataforma para los enemigos

---
+++ Assets/juan/Script/MovePlataform.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlataform : MonoBehaviour
+{
+
+    [SerializeField] private float speed,limit;
+    [SerializeField] private bool moveRight;
+    [SerializeField] private Transform groundController;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D groundInfo=Physics2D.Raycast(groundController.position,Vector2.down,limit);
+        rb.velocity=new Vector2(speed,rb.velocity.y);
+
+        if (groundInfo == false)
+        {
+            //Girar
+            Girar();
+        }
+
+    }
+
+    private void Girar()
+    {
+        moveRight = !moveRight;
+        transform.eulerAngles= new Vector3(0,transform.eulerAngles.y+180,0);
+        speed *= -1;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundController.transform.position,groundController.transform.position+ Vector3.down*limit);
+    }
+}
